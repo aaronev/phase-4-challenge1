de5a591e5bb1571b9bf5dc7cf4fb3f827f88b3b6
Aaron Villanueva <aarone.villanueva@gmail.com>
2017-09-18 11:23:02 -0700

    builds db, db functions, and models abstract functions

--- src/models/db/schema.sql
+++ src/models/db/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  image VARCHAR(255), 
+  image VARCHAR(255) DEFAULT '/img/no-dj.png', 
   timestamp TIMESTAMP DEFAULT current_timestamp
 );
 
---
+++ src/models/users.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt')
+const db = require('./db/db.js')
+
+const users = {}
+
+encryptText = (plainText) =>
+  bcrypt.hashSync(plainText, bcrypt.genSaltSync(10))
+
+users.validPassword = (plainText, hashedText) => 
+  bcrypt.compareSync(plainText, hashedText)
+
+users.all = () => 
+  db.all('users')
+
+users.create = (name, email, password) => 
+  db.create(
+    'users', 
+    ['name', 'email', 'password'], 
+    '($1, $2, $3)', 
+    [name, email, encryptText(password)]
+  )
+
+users.findByID = ID => 
+  db.find('users', 'id', ID)
+  .then(users => users[0])
+
+users.findByEmail = email => 
+  db.find('users', 'email', email)
+  .then(users => users[0])
+
+module.exports = users
