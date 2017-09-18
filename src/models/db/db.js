ed10ff2baf5f608d205d00004fcc9f2a477e6b45
Aaron Villanueva <aarone.villanueva@gmail.com>
2017-09-18 10:44:41 -0700

    adds sql injections creating generic db functions for models/abstract functions

--- src/models/db/db.js
+++ src/models/db/db.js
@@ -1,35 +1,58 @@
-const pg = require('pg')
-
+const pgp = require('pg-promise')()
 const dbName = 'vinyl'
 const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
-const client = new pg.Client(connectionString)
+const db = pgp(connectionString)
 
-client.connect()
+const SQLInjections = {}
 
-function getAlbums(cb) {
-  _query('SELECT * FROM albums', [], cb)
-}
-
-function getAlbumsByID(albumID, cb) {
-  _query('SELECT * FROM albums WHERE id = $1', [albumID], cb)
-}
-
-function _query(sql, variables, cb) {
-  console.log('QUERY ->', sql.replace(/[\n\s]+/g, ' '), variables)
-
-  client.query(sql, variables, (error, result) => {
-    if (error) {
-      console.log('QUERY -> !!ERROR!!')
-      console.error(error)
-      cb(error)
-    } else {
-      console.log('QUERY ->', JSON.stringify(result.rows))
-      cb(error, result.rows)
-    }
+SQLInjections.all = table => 
+  db.any(`
+    SELECT * 
+    FROM ${table} 
+    ORDER BY timestamp DESC`)
+  .catch(error => { 
+    console.log('all = () => ', error)
+    throw error
   })
-}
 
-module.exports = {
-  getAlbums,
-  getAlbumsByID,
-}
+SQLInjections.find = (table, column, value) => 
+  db.any(`
+    SELECT * 
+    FROM ${table} 
+    WHERE ${column}=$1 
+    ORDER BY timestamp DESC`, value)
+  .catch(error => {
+    console.log('find = () => ', error)
+    throw error
+  })
+
+SQLInjections.create = (table, columnsArray, $numsWithParens, valueArray) => 
+  db.any(`
+    INSERT INTO ${table} (${columnsArray})
+    VALUES ${$nums}`, valueArray)
+  .catch(error => {
+    console.log('create = () => ', error)
+    throw error
+  })
+
+SQLInjections.delete = (table, column, value) => 
+  db.any(`
+    DELETE FROM ${table}
+    WHERE ${column}=$1`, value)
+  .catch(error => {
+    console.log('delete = () => ', error)
+    throw error
+  })
+
+SQLInjections.limit = (table, limit) => 
+  db.any(`
+    SELECT * 
+    FROM ${table}
+    ORDER BY timestamp DESC
+    LIMIT $1`, limt)
+  .catch(error => {
+    console.log('limit = () => ', error)
+    throw error
+  })
+
+module.exports = SQLInjections
