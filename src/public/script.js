ccadf738507bf5ae40654c82e7fa7e8d8379cb7d
Aaron Villanueva <aarone.villanueva@gmail.com>
2017-09-19 08:39:33 -0700

    adds class to album and reviews for styling and front end js

--- src/public/script.js
+++ src/public/script.js
@@ -1,6 +1,5 @@
 console.log('hello from the browser JavaScript')
 
-
 function confirmDelete(reviewID) {
   if (confirm('Are you sure you want to delete this review?')) {
     fetch('/reviews/' + reviewID, {
